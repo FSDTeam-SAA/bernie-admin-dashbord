408c69f2977766e5a5d4142905e4bc266b16cffa
Tanvir Ahmmed <ahmmedtanvir009@gmail.com>
2026-06-06 10:57:02 +0600

    new changes

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -8,7 +8,7 @@ import React from 'react'
 function page() {
   return (
     <div>
-      <DashboardHeader title="Dashboard Overview" />
+      <DashboardHeader title="Dashboard Overviews" />
       <OverviewCard />
       <div className="mt-6 grid grid-cols-1 items-stretch gap-5 xl:grid-cols-[minmax(0,2fr)_minmax(320px,1fr)]">
         <EarningOverview />
